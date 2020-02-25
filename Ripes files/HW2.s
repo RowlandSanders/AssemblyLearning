d638c30d408eed71aa5018b553922eb73118b8e0
RowlandSanders <48600494+RowlandSanders@users.noreply.github.com>
2020-02-25 14:17:39 -0800

    Attempt 2 and loop assembly

--- Ripes files/HW2.s
+++ Ripes files/HW2.s
@@ -1,38 +1,35 @@
-.data
+.data 
 A: .word 0,1,2,3,4,5
 B: .word 0,0,0,0,0,0
 .text
 li x5, 6
-li x6,2 
+li x6, 2
 la x10, A
 la x11, B
 li x7, 0
 li x29, 0
 
-start:
-add x7, x0, x0
-add x29, x0, x0
-addi x4, x0, 4
+sw x0, 0(s0)
+addi s1, x0, 0
+sw x1, 0(s2)
+addi s3,x1,0
+addi x3, x0, 4
 
-loopji:
-beq x7, x5,END
-beq x29, x6, loopji
-mul x1, x29, x4
-mul x8, x7, x3
-addi x29, x29, 1
-addi x7,x7,1
-lw x28, x1(x11)
+loopi:
+beq x7, x29, ENDi
+mul x29,x29,x3
+add x7, x7, x29
+add s3, s1, x29
+jal x6,loopi
 
+loopj: beq x7, x7, ENDj
+jal x5,loopi
 
-END:
+ENDj:
+addi x7, x7,1
+jal x0, loopi
 
-
-
-
-
-
-
-
-
-
+ENDi:
+	exit:
+	   nop
 
